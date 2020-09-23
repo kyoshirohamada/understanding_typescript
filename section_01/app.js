7beaa720162cc31bd72d1bd68ecd3ede058562f1
Johan Gunawan <jgunawan.code@gmail.com>
2020-09-23 08:27:05 +0700

    Section 2.19

--- section_01/app.js
+++ section_01/app.js
@@ -1,9 +1,12 @@
 var person = {
     name: 'Johan',
     age: 33,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 };
 console.log(person.name);
+//person.role.push('admin');
+person.role = [0, 'admin'];
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
--- section_01/app.ts
+++ section_01/app.ts
@@ -1,10 +1,19 @@
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
     name: 'Johan',
     age: 33,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 }
 
 console.log(person.name);
+
+//person.role.push('admin');
+person.role = [0, 'admin'];
 
 for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());    
