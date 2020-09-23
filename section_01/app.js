4e6e42bf2844e49bcdc64236d4cd0fab3a150632
Johan Gunawan <jgunawan.code@gmail.com>
2020-09-23 08:18:55 +0700

    Section 2.18

--- section_01/app.js
+++ section_01/app.js
@@ -1,5 +1,10 @@
 var person = {
     name: 'Johan',
-    age: 33
+    age: 33,
+    hobbies: ['Sports', 'Cooking']
 };
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- section_01/app.ts
+++ section_01/app.ts
@@ -1,6 +1,11 @@
 const person = {
     name: 'Johan',
-    age: 33
+    age: 33,
+    hobbies: ['Sports', 'Cooking']
 }
 
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());    
+}
