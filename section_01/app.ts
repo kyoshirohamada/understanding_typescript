b3924b5b33580331f3bd92043f982113508564de
Johan Gunawan <jgunawan.code@gmail.com>
2020-09-26 22:18:49 +0700

    Section 2.22

--- section_01/app.ts
+++ section_01/app.ts
@@ -1,24 +1,17 @@
-// const person: {
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role: [number, string];
-// } = {
-//     name: 'Johan',
-//     age: 33,
-//     hobbies: ['Sports', 'Cooking'],
-//     role: [2, 'author']
-// }
-
-enum Role { ADMIN, READ_ONLY, AUTHOR };
-
-const person = {
-    name: 'Johan',
-    age: 33,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN
+function combine(input1: number | string, input2: number | string) {
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    
+    return result;
 }
 
-for (const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase());    
-}
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
+
+const combinedNames = combine('Max', 'Anna');
+console.log(combinedNames)
