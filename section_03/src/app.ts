e28674a64e4cf588394da419c969e26382e62e7f
Johan Gunawan <jgunawan.code@gmail.com>
2020-10-02 17:23:40 +0700

    Section 5.61

--- section_03/src/app.ts
+++ section_03/src/app.ts
@@ -1,2 +1,17 @@
-const userName = 'Johan';
- console.log(userName);
+class Department {
+    name: string;
+
+    constructor(n: string) {
+        this.name = n;
+    }
+
+    describe() {
+        console.log('Department: ' + this.name);
+    }
+}
+
+const accounting = new Department('Accounting');
+
+console.log(accounting);
+
+accounting.describe();
