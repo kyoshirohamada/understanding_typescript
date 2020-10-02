58451a15a6283d56fa30fe5b2f3032335d10cfd9
Johan Gunawan <jgunawan.code@gmail.com>
2020-10-02 18:50:16 +0700

    Section 5.65

--- section_03/src/app.ts
+++ section_03/src/app.ts
@@ -1,7 +1,7 @@
 class Department {    
     private employees: string[] = [];
 
-    constructor(private id: string, private name: string) {        
+    constructor(private id: string, public name: string) {        
     }
 
     describe(this: Department) {
@@ -11,10 +11,46 @@ class Department {
     addEmployee(employee: string) {
         this.employees.push(employee);
     }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 
-const accounting = new Department('d1', 'Accounting');
+class ITDepartment extends Department {  
+    admins: string[];  
+    constructor(id: string, admins: string []) {
+        super(id, 'IT');
+        this.admins = admins;
+    }
+}
 
-console.log(accounting);
+class AccountingDepartment extends Department {      
+    constructor(id: string, private reports: string []) {
+        super(id, 'Accounting');       
+    }
 
-accounting.describe();
+    addReport(text: string) {
+        this.reports.push(text);
+    }
+
+    printReports() {
+        console.log(this.reports);
+    }
+}
+
+const IT = new ITDepartment('d1', ['Max']);
+
+IT.addEmployee('Max');
+IT.addEmployee('Manu');
+IT.describe();
+
+IT.name = 'NEW Name';
+IT.printEmployeeInformation();
+
+console.log(IT);
+
+const accounting = new AccountingDepartment('d2', []);
+accounting.addReport('Something went wrong...');
+accounting.printReports();
