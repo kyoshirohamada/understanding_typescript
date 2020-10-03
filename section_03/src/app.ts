0e89e92d13a0240943cb84f5d6f24415977abc07
Johan Gunawan <jgunawan.code@gmail.com>
2020-10-03 22:31:46 +0700

    Section 6.85

--- section_03/src/app.ts
+++ section_03/src/app.ts
@@ -1,23 +1,65 @@
-interface Greetable {
-    name: string   
-
-    greet(phrase: string): void;
-}
-
-class Person implements Greetable {
+type Admin = {
     name: string;
+    privileges: string[];
+};
 
-    constructor(n: string) {
-        this.name = n;
+type Employee = {
+    name: string;
+    startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+    name: 'Max',
+    privileges: ['create-server'],
+    startDate: new Date()
+};
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+    console.log('Name: ' + emp.name);
+
+    if ('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges);
     }
 
-    greet(phrase: string) {
-        console.log(phrase + ' ' + this.name);
+    if ('startDate' in emp) {
+        console.log('Start Date: ' + emp.startDate);
+    }    
+}
+
+printEmployeeInformation(e1);
+
+class Car {
+    drive() {
+        console.log('Driving...');
     }
 }
 
-let user1: Greetable;
+class Truck {
+    drive() {
+        console.log('Driving a truck...');
+    }
 
-user1 = new Person('Max');
+    loadCargo(amount: number) {
+        console.log('Loading cargo ...' + amount);
+    }
+}
 
-user1.greet('Hi there I am');
+type Vehicle =  Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+    vehicle.drive();
+
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+
+useVehicle(v1);
+useVehicle(v2);
