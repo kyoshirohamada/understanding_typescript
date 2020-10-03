6a00cb97e49808cf7f83dc325d60398b833fd7ee
Johan Gunawan <jgunawan.code@gmail.com>
2020-10-03 23:27:15 +0700

    Section 7.96

--- section_03/src/app.ts
+++ section_03/src/app.ts
@@ -1,6 +1,6 @@
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
     return Object.assign(objA, objB);
 }
 
-const mergeObj = merge({name: 'Max'}, {age: 30});
+const mergeObj = merge({name: 'Max', hobbies: ['Sports']}, {age: 30});
 console.log(mergeObj.name);
