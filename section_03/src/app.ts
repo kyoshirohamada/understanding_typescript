156937b753cc31a66cc82b20ce0eb235e53b993f
Johan Gunawan <jgunawan.code@gmail.com>
2020-10-03 23:34:34 +0700

    Section 7.97

--- section_03/src/app.ts
+++ section_03/src/app.ts
@@ -4,3 +4,20 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergeObj = merge({name: 'Max', hobbies: ['Sports']}, {age: 30});
 console.log(mergeObj.name);
+
+interface Lengthy {
+    length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T) {
+    let descriptionText = 'Got no value.';
+
+    if (element.length === 1) {
+        descriptionText = 'Got 1 element';
+    } else if (element.length > 1) {
+        descriptionText = 'Got ' + element.length + ' elements.';
+    }
+    return [element, descriptionText];
+}
+
+console.log(countAndDescribe(['Sports', 'Cooking']));
