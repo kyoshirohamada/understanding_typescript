4534dd836856441e4bb4c306ee24964973a8c0ad
Johan Gunawan <jgunawan.code@gmail.com>
2020-10-03 22:49:04 +0700

    Section 6.87

--- section_03/src/app.ts
+++ section_03/src/app.ts
@@ -95,3 +95,12 @@ moveAnimal({type: 'bird', flyingSpeed: 1000});
 const userInputElement = document.getElementById('user-input')! as HTMLInputElement;
 
 userInputElement.value = 'Hi there!';
+
+interface ErrorContainer { // { email: 'Not a valid email', username: 'Must start with character' }    
+    [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+    email: 'Not a valid email',
+    username: 'Must start with a capital character!'
+}
