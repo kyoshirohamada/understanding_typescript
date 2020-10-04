549b820014ddfee396ad96450d84f05121e7193f
Johan Gunawan <jgunawan.code@gmail.com>
2020-10-04 22:36:14 +0700

    Section 8.110

--- section_03/src/app.ts
+++ section_03/src/app.ts
@@ -1,31 +1,65 @@
-function Logger(logString: string) {
-    return function(constructor: Function) {
-        console.log(logString);
-        console.log(constructor);
-    }    
-}
+// function Logger(logString: string) {
+//     console.log('Logger Factory');
+//     return function(constructor: Function) {
+//         console.log(logString);
+//         console.log(constructor);
+//     }    
+// }
 
-function WithTemplate(template: string, hookId: string) {
-    return function(constructor: any) {
-        const hookEl = document.getElementById(hookId);
-        const p = new constructor();
-        if (hookEl) {
-            hookEl.innerHTML = template;
-            hookEl.querySelector('h1')!.textContent = p.name;
-        }
-    }
-}
+// function WithTemplate(template: string, hookId: string) {
+//     console.log('WithTemplate Factory');
+//     return function(constructor: any) {
+//         console.log('Rendering template...');
+//         const hookEl = document.getElementById(hookId);
+//         const p = new constructor();
+//         if (hookEl) {
+//             hookEl.innerHTML = template;
+//             hookEl.querySelector('h1')!.textContent = p.name;
+//         }
+//     }
+// }
 
-//@Logger('LOGGING - PERSON')
-@WithTemplate('<h1>My Person Object</h1>', 'app')
-class Person {
-    name = 'Johan';
+// @Logger('LOGGING - PERSON')
+// @WithTemplate('<h1>My Person Object</h1>', 'app')
+// class Person {
+//     name = 'Johan';
 
-    constructor() {
-        console.log('Creating person object...');
-    }
-}
+//     constructor() {
+//         console.log('Creating person object...');
+//     }
+// }
 
 // const person = new Person();
 
 // console.log(person);
+
+// =================================
+
+function Log(target: any, propertyName: string) {
+    console.log('Property decorator!');
+    console.log(target, propertyName);
+}
+
+class Product {
+    @Log
+    title: string;
+    private _price: number;
+
+    set price(val: number) {
+        if (val > 0) {
+            this._price = val;
+        }
+        else {
+            throw new Error('Invalid price - should be positive!');
+        }
+    }
+
+    constructor(t: string, p: number) {
+        this.title = t;
+        this._price = p;
+    }
+
+    getPriceWithTax(tax: number) {
+        return this._price * (1 + tax);
+    }
+}
