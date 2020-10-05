246b64db8b7d143d98349238ec8053ba6284eb93
Johan Gunawan <jgunawan.code@gmail.com>
2020-10-05 20:54:43 +0700

    Section 8.115

--- section_03/src/app.ts
+++ section_03/src/app.ts
@@ -1,92 +1,120 @@
-function Logger(logString: string) {
-    console.log('Logger Factory');
-    return function(constructor: Function) {
-        console.log(logString);
-        console.log(constructor);
-    }    
-}
+// function Logger(logString: string) {
+//     console.log('Logger Factory');
+//     return function(constructor: Function) {
+//         console.log(logString);
+//         console.log(constructor);
+//     }    
+// }
 
-function WithTemplate(template: string, hookId: string) {
-    console.log('WithTemplate Factory');
-    return function<T extends { new(...args: any[]): { name: string }}>(
-        originalConstructor: T) {
-        console.log('Rendering template...');        
-        return class extends originalConstructor {
-            constructor(..._: any[]) {
-                super();
-                const hookEl = document.getElementById(hookId);
-                // const p = new originalConstructor();
-                if (hookEl) {
-                    hookEl.innerHTML = template;
-                    hookEl.querySelector('h1')!.textContent = this.name;
-                }
-            }
-        }
-    }
-}
+// function WithTemplate(template: string, hookId: string) {
+//     console.log('WithTemplate Factory');
+//     return function<T extends { new(...args: any[]): { name: string }}>(
+//         originalConstructor: T) {
+//         console.log('Rendering template...');        
+//         return class extends originalConstructor {
+//             constructor(..._: any[]) {
+//                 super();
+//                 const hookEl = document.getElementById(hookId);
+//                 // const p = new originalConstructor();
+//                 if (hookEl) {
+//                     hookEl.innerHTML = template;
+//                     hookEl.querySelector('h1')!.textContent = this.name;
+//                 }
+//             }
+//         }
+//     }
+// }
 
-@Logger('LOGGING - PERSON')
-@WithTemplate('<h1>My Person Object</h1>', 'app')
-class Person {
-    name = 'Johan';
+// @Logger('LOGGING - PERSON')
+// @WithTemplate('<h1>My Person Object</h1>', 'app')
+// class Person {
+//     name = 'Johan';
 
-    constructor() {
-        console.log('Creating person object...');
-    }
-}
+//     constructor() {
+//         console.log('Creating person object...');
+//     }
+// }
 
 // const person = new Person();
 // console.log(person);
 
 // =================================
 
-// function Log(target: any, propertyName: string) {
-//     console.log('Property decorator!');
-//     console.log(target, propertyName);
-// }
+function Log(target: any, propertyName: string) {
+    console.log('Property decorator!');
+    console.log(target, propertyName);
+}
 
-// function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
-//     console.log('Accessor decorator!');
-//     console.log(target);
-//     console.log(name);
-//     console.log(descriptor);
-// }
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+    console.log('Accessor decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
 
-// function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
-//     console.log('Method decorator!');
-//     console.log(target);
-//     console.log(name);
-//     console.log(descriptor);
-// }
-// function Log4(target: any, name: string | Symbol, position: number) {
-//     console.log('Parameter decorator!');
-//     console.log(target);
-//     console.log(name);
-//     console.log(position);
-// }
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+    console.log('Method decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log4(target: any, name: string | Symbol, position: number) {
+    console.log('Parameter decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
 
-// class Product {
-//     @Log
-//     title: string;
-//     private _price: number;
+class Product {
+    //@Log
+    title: string;
+    private _price: number;
 
-//     @Log2
-//     set price(val: number) {
-//         if (val > 0) {
-//             this._price = val;
-//         }
-//         else {
-//             throw new Error('Invalid price - should be positive!');
-//         }
-//     }
+    //@Log2
+    set price(val: number) {
+        if (val > 0) {
+            this._price = val;
+        }
+        else {
+            throw new Error('Invalid price - should be positive!');
+        }
+    }
 
-//     constructor(t: string, p: number) {
-//         this.title = t;
-//         this._price = p;
-//     }
+    constructor(t: string, p: number) {
+        this.title = t;
+        this._price = p;
+    }
 
-//     // @Log3
-//     // getPriceWithTax(@Log4 t ax: number) {
-//     //     return this._price * (1 + tax);
-//     // }
-// }
+    //@Log3
+    getPriceWithTax(tax: number) {
+        return this._price * (1 + tax);
+    }
+}
+
+function Autobind(_:any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+
+    return adjDescriptor;
+}
+
+class Printer {
+    message = 'This works!';
+
+    @Autobind
+    showMessage() {
+        console.log(this.message);
+    }
+}
+
+const p = new Printer();
+
+const button = document.querySelector('button')!;
+button.addEventListener('click', p.showMessage);
