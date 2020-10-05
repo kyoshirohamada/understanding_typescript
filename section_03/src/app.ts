3c828284cf595382fcf964d20025c5fcb2ca7d24
Johan Gunawan <jgunawan.code@gmail.com>
2020-10-05 22:04:45 +0700

    Section 8.116

--- section_03/src/app.ts
+++ section_03/src/app.ts
@@ -38,83 +38,157 @@
 // const person = new Person();
 // console.log(person);
 
+// // =================================
+
+// function Log(target: any, propertyName: string) {
+//     console.log('Property decorator!');
+//     console.log(target, propertyName);
+// }
+
+// function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+//     console.log('Accessor decorator!');
+//     console.log(target);
+//     console.log(name);
+//     console.log(descriptor);
+// }
+
+// function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+//     console.log('Method decorator!');
+//     console.log(target);
+//     console.log(name);
+//     console.log(descriptor);
+// }
+// function Log4(target: any, name: string | Symbol, position: number) {
+//     console.log('Parameter decorator!');
+//     console.log(target);
+//     console.log(name);
+//     console.log(position);
+// }
+
+// class Product {
+//     //@Log
+//     title: string;
+//     private _price: number;
+
+//     //@Log2
+//     set price(val: number) {
+//         if (val > 0) {
+//             this._price = val;
+//         }
+//         else {
+//             throw new Error('Invalid price - should be positive!');
+//         }
+//     }
+
+//     constructor(t: string, p: number) {
+//         this.title = t;
+//         this._price = p;
+//     }
+
+//     //@Log3
+//     getPriceWithTax(tax: number) {
+//         return this._price * (1 + tax);
+//     }
+// }
+
+// function Autobind(_:any, _2: string, descriptor: PropertyDescriptor) {
+//     const originalMethod = descriptor.value;
+//     const adjDescriptor: PropertyDescriptor = {
+//         configurable: true,
+//         enumerable: false,
+//         get() {
+//             const boundFn = originalMethod.bind(this);
+//             return boundFn;
+//         }
+//     };
+
+//     return adjDescriptor;
+// }
+
+// class Printer {
+//     message = 'This works!';
+
+//     @Autobind
+//     showMessage() {
+//         console.log(this.message);
+//     }
+// }
+
+// const p = new Printer();
+
+// const button = document.querySelector('button')!;
+// button.addEventListener('click', p.showMessage);
+
 // =================================
 
-function Log(target: any, propertyName: string) {
-    console.log('Property decorator!');
-    console.log(target, propertyName);
+interface ValidatorConfig {
+    [property: string]: {
+        [validatableProp: string]: string[] // ['required', 'positive']
+    }
 }
 
-function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
-    console.log('Accessor decorator!');
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) {
+    registeredValidators[target.constructor.name] = {
+        [propName]: ['required']
+    };
 }
 
-function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
-    console.log('Method decorator!');
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
-}
-function Log4(target: any, name: string | Symbol, position: number) {
-    console.log('Parameter decorator!');
-    console.log(target);
-    console.log(name);
-    console.log(position);
+function PositiveNumber(target: any, propName: string) {
+    registeredValidators[target.constructor.name] = {
+        [propName]: ['positive']
+    };
 }
 
-class Product {
-    //@Log
-    title: string;
-    private _price: number;
-
-    //@Log2
-    set price(val: number) {
-        if (val > 0) {
-            this._price = val;
-        }
-        else {
-            throw new Error('Invalid price - should be positive!');
+function validate(obj: any) {
+    const objValidatorConfig = registeredValidators[obj.constructor.name];
+    if (!objValidatorConfig) {
+        return true;
+    }
+    let isValid = true;
+    for (const prop in objValidatorConfig) {
+        for(const validator of objValidatorConfig[prop]) {
+            switch (validator) {
+                case 'required':
+                    isValid = isValid && !!obj[prop];
+                    break;
+                case 'positive':
+                    isValid = isValid && obj[prop] > 0;
+                    break;                    
+            }
         }
     }
+
+    return isValid;
+} 
+
+class Course {
+    @Required
+    title: string;
+    @PositiveNumber
+    price: number;
 
     constructor(t: string, p: number) {
         this.title = t;
-        this._price = p;
-    }
-
-    //@Log3
-    getPriceWithTax(tax: number) {
-        return this._price * (1 + tax);
+        this.price = p;
     }
 }
 
-function Autobind(_:any, _2: string, descriptor: PropertyDescriptor) {
-    const originalMethod = descriptor.value;
-    const adjDescriptor: PropertyDescriptor = {
-        configurable: true,
-        enumerable: false,
-        get() {
-            const boundFn = originalMethod.bind(this);
-            return boundFn;
-        }
-    };
+const courseForm = document.querySelector('form')!;
+courseForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const titleEl = document.getElementById('title') as HTMLInputElement;
+    const priceEl = document.getElementById('price') as HTMLInputElement;
 
-    return adjDescriptor;
-}
+    const title = titleEl.value;
+    const price = +priceEl.value;
 
-class Printer {
-    message = 'This works!';
+    const createdCourse = new Course(title, price);
 
-    @Autobind
-    showMessage() {
-        console.log(this.message);
+    if (!validate(createdCourse)) {
+        alert('Invalid input, please try again!');
+        return;
     }
-}
-
-const p = new Printer();
-
-const button = document.querySelector('button')!;
-button.addEventListener('click', p.showMessage);
+    console.log(createdCourse);
+});
